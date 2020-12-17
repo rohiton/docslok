57627313f7dba4dece4460806f4cb2e2a3e91270
Rohit Suthar <rohitkumar1121.rk@gmail.com>
2020-12-17 12:09:39 +0530

    DB Script Updated

--- sql/db-dump.sql
+++ sql/db-dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: docslok
+-- Host: 127.0.0.1    Database: DOCSLOK
 -- ------------------------------------------------------
 -- Server version	8.0.21-0ubuntu0.20.04.4
 
@@ -16,29 +16,115 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `emailconfirmation`
+-- Table structure for table `EMAIL_CONFIRMATION`
 --
 
-DROP TABLE IF EXISTS `emailconfirmation`;
+DROP TABLE IF EXISTS `EMAIL_CONFIRMATION`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `emailconfirmation` (
-  `token_id` bigint NOT NULL,
-  `confirmation_token` varchar(255) DEFAULT NULL,
-  `created_date` datetime DEFAULT NULL,
-  `user_id` int NOT NULL,
-  PRIMARY KEY (`token_id`),
-  KEY `FKf0l63xjgfcgb4675vsxkrssmg` (`user_id`)
+CREATE TABLE `EMAIL_CONFIRMATION` (
+  `TOKEN_ID` bigint NOT NULL,
+  `CONFIRMATION_TOKEN` varchar(255) DEFAULT NULL,
+  `CREATION_DATE` datetime DEFAULT NULL,
+  `USER_ID` int NOT NULL,
+  PRIMARY KEY (`TOKEN_ID`),
+  KEY `FKijpqdtw369di69tnx0wu6piy6` (`USER_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `emailconfirmation`
+-- Dumping data for table `EMAIL_CONFIRMATION`
 --
 
-LOCK TABLES `emailconfirmation` WRITE;
-/*!40000 ALTER TABLE `emailconfirmation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `emailconfirmation` ENABLE KEYS */;
+LOCK TABLES `EMAIL_CONFIRMATION` WRITE;
+/*!40000 ALTER TABLE `EMAIL_CONFIRMATION` DISABLE KEYS */;
+INSERT INTO `EMAIL_CONFIRMATION` VALUES (9,'8bfe8dd2-34a9-45d7-bb41-74ed9288cdff','2020-12-17 11:42:01',8);
+/*!40000 ALTER TABLE `EMAIL_CONFIRMATION` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ROLES`
+--
+
+DROP TABLE IF EXISTS `ROLES`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ROLES` (
+  `ROLE_ID` int NOT NULL,
+  `ROLE` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ROLE_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ROLES`
+--
+
+LOCK TABLES `ROLES` WRITE;
+/*!40000 ALTER TABLE `ROLES` DISABLE KEYS */;
+INSERT INTO `ROLES` VALUES (1,'ADMIN'),(2,'USER');
+/*!40000 ALTER TABLE `ROLES` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `USERS`
+--
+
+DROP TABLE IF EXISTS `USERS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `USERS` (
+  `USER_ID` int NOT NULL,
+  `AADHAAR_NO` bigint DEFAULT NULL,
+  `ACCOUNT_STATUS` varchar(255) DEFAULT NULL,
+  `EMAIL` varchar(128) DEFAULT NULL,
+  `FIRST_NAME` varchar(255) DEFAULT NULL,
+  `IS_ACTIVE` bit(1) DEFAULT NULL,
+  `IS_EMAIL_VERIFIED` bit(1) DEFAULT NULL,
+  `LAST_NAME` varchar(255) DEFAULT NULL,
+  `PASSWORD` varchar(255) DEFAULT NULL,
+  `SECRET_PIN` int DEFAULT NULL,
+  `USERNAME` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`USER_ID`),
+  UNIQUE KEY `UK_ixd0ik0k68d42ooo2wxi1c95b` (`AADHAAR_NO`),
+  UNIQUE KEY `UK_81nqioeq3njjrwqaltk2mcobj` (`EMAIL`),
+  UNIQUE KEY `UK_h6k33r31i2nvrri9lok4r163j` (`USERNAME`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USERS`
+--
+
+LOCK TABLES `USERS` WRITE;
+/*!40000 ALTER TABLE `USERS` DISABLE KEYS */;
+INSERT INTO `USERS` VALUES (8,298154791755,'ACTIVE','Rohitkumar1121.rk@gmail.com','Rohit',_binary '',_binary '','Suthar','$2a$10$OqVgW19bZ58xQSlaXb0v5OQ08h3acVyJWwUnEWrchDiEROdVS6ZMe',451846,'rohiton');
+/*!40000 ALTER TABLE `USERS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `USER_ROLE`
+--
+
+DROP TABLE IF EXISTS `USER_ROLE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `USER_ROLE` (
+  `USER_ID` int NOT NULL,
+  `ROLE_ID` int NOT NULL,
+  PRIMARY KEY (`USER_ID`,`ROLE_ID`),
+  KEY `FKcv7h6lq93jnxs6trp7xq1x5sn` (`ROLE_ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USER_ROLE`
+--
+
+LOCK TABLES `USER_ROLE` WRITE;
+/*!40000 ALTER TABLE `USER_ROLE` DISABLE KEYS */;
+INSERT INTO `USER_ROLE` VALUES (8,2);
+/*!40000 ALTER TABLE `USER_ROLE` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -59,87 +145,8 @@ CREATE TABLE `hibernate_sequence` (
 
 LOCK TABLES `hibernate_sequence` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (33),(33);
+INSERT INTO `hibernate_sequence` VALUES (10),(10),(10);
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `roles` (
-  `role_id` int NOT NULL,
-  `role` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`role_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `roles`
---
-
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'ADMIN'),(2,'USER');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_role`
---
-
-DROP TABLE IF EXISTS `user_role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_role` (
-  `user_id` int NOT NULL,
-  `role_id` int NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
-  KEY `FKt7e7djp752sqn6w22i6ocqy6q` (`role_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_role`
---
-
-LOCK TABLES `user_role` WRITE;
-/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `user_id` int NOT NULL,
-  `aadhaar_no` int DEFAULT NULL,
-  `active` bit(1) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `firstname` varchar(255) DEFAULT NULL,
-  `lastname` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `account_status` varchar(30) DEFAULT NULL,
-  `email_verified` bit(1) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -151,4 +158,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-14 18:29:01
+-- Dump completed on 2020-12-17 12:07:14
